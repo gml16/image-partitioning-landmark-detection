0b2e6baa9df86b64b8a98073725afe3240df3727
Guy Leroy <g.m.leroy@outlook.com>
2020-10-03 19:03:53 +0100

    visualising labels in predict.py

--- predict.py
+++ predict.py
@@ -31,9 +31,8 @@ def predict_true_label():
     view_image(y)
 
 
-def predict_using_model(file_paths, landmark, separator, model_path):
-    x, y = get_data(file_paths, None, landmark_wanted=landmark, separator=separator)
-    view_image(x)
+def predict_using_model(file_paths, landmark_paths, landmark, separator, model_path):
+    x, labels = get_data(file_paths, landmark_paths, landmark_wanted=landmark, separator=separator)
 
     unet = UNet(in_dim=1, out_dim=6, num_filters=4)
     unet.load_state_dict(torch.load(model_path))
@@ -45,26 +44,29 @@ def predict_using_model(file_paths, landmark, separator, model_path):
     for i, tx in enumerate(tensor_x):
         tx = tx.unsqueeze(0)
         view_image(tx.cpu())
-        y = unet(tx).cpu()
+        view_image(labels[i])
+        out = unet(tx).cpu()
 
-        landmark = predict(y)
+        landmark = predict(out)
         print(f"landmark {i}", landmark)
 
-        action_map = y.detach().numpy().squeeze()
+        action_map = out.detach().numpy().squeeze()
         action_map = np.argmax(action_map, axis=0)
         view_image(action_map)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--file_paths',
-                        default="data/files.txt")
+                        default="data/files_one.txt")
+    parser.add_argument('--landmark_paths',
+                        default="data/landmarks_one.txt")
     parser.add_argument('--model_path')
     parser.add_argument('--separator', default=",")
     parser.add_argument('--landmark', type=int, default=0)
     args = parser.parse_args()
 
-    predict_true_label()
-    #predict_using_model(args.file_paths, args.landmark, args.separator, args.model_path)
+    #predict_true_label()
+    predict_using_model(args.file_paths, args.landmark_paths, args.landmark, args.separator, args.model_path)
     
 
 if __name__ == "__main__":
